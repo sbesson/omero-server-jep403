831383973b50e959fd9cb3bb0862816843f0d476
Sébastien Besson <sbesson@glencoesoftware.com>
2024-04-29 11:58:05 +0100

    Add links to JDK 17 strong encapsulation migration guide

--- jdk21/init.sh
+++ jdk21/init.sh
@@ -10,6 +10,7 @@ psql -h pg -U omero jdk21 -f OMERO.sql
 
 omero config set omero.db.host pg
 omero config set omero.db.name jdk21
+# See https://docs.oracle.com/en/java/javase/17/migrate/migrating-jdk-8-later-jdk-releases.html#GUID-7BB28E4D-99B3-4078-BDC4-FC24180CE82B
 omero config set omero.jvmcfg.append '--add-opens java.base/java.lang=ALL-UNNAMED --add-exports java.naming/com.sun.jndi.ldap=ALL-UNNAMED'
 omero certificates
 
